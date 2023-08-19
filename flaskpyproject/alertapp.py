eb3ad052e896e0d74ea10c4043583a46a4767011
Krithika2511 <krithisuji@gmail.com>
2023-08-19 16:58:28 +0530

    updated path

--- flaskpyproject/alertapp.py
+++ flaskpyproject/alertapp.py
@@ -5,13 +5,14 @@ app = Flask(__name__)
 json_files = []
 directory = "Test"
 print(directory)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("current path", dir_path)
 # Sample JSON data for the machines
 for filename in os.listdir(directory):
     if filename.endswith(".json"):
         file = open(directory + "/" + filename)
         json_files.append(json.load(file))                         
-dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
+
 @app.route('/')
 def index():
     return render_template('index.html', data=json_files)

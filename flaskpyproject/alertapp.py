12e9038018b17ecc96cc80b75c3a5cd7f8cc6c16
Krithika2511 <krithisuji@gmail.com>
2023-08-19 17:13:34 +0530

    moved test

--- flaskpyproject/alertapp.py
+++ flaskpyproject/alertapp.py
@@ -1,25 +1,22 @@
-# from flask import Flask, render_template
-# import json
-# import os 
-# app = Flask(__name__)
-# json_files = []
-testpath = "C:\ProgramData\Jenkins\.jenkins\workspace\dashboard\wimsrepo\flaskpyproject"
+from flask import Flask, render_template
+import json
+import os 
+app = Flask(__name__)
+json_files = []
 directory = "Test"
-print(testpath.split("\\"))
+print(directory)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("current path", dir_path)
+# Sample JSON data for the machines
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):
+        file = open(directory + "/" + filename)
+        json_files.append(json.load(file))                         
 
-# print(directory)
-# dir_path = os.path.dirname(os.path.realpath(__file__))
-# print("current path", dir_path)
-# # Sample JSON data for the machines
-# for filename in os.listdir(directory):
-#     if filename.endswith(".json"):
-#         file = open(directory + "/" + filename)
-#         json_files.append(json.load(file))                         
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html', data=json_files)
+@app.route('/')
+def index():
+    return render_template('index.html', data=json_files)
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
